6235e6f6de7ef2c05a99c5cfbe3bddb8ce94dcd8
Ariel Xu <arielxu@live.com>
2020-11-18 13:57:39 +0800

    created two controllers for resto and reviews

--- db/migrate/20201118055101_create_reviews.rb
+++ db/migrate/20201118055101_create_reviews.rb
@@ -1,8 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-      t.string :rating
-      t.string :content
+      t.integer :rating
+      t.text :content
+      t.references :restaurant, null: false, foreign_key: true
 
       t.timestamps
     end
