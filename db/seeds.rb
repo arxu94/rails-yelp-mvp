db739566d94e98e888a86141cea9a94d9482624a
Ariel Xu <arielxu@live.com>
2020-11-18 16:27:27 +0800

    created views for index of resto, new resto form, singular resto and ratings/review for resto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all restaurants"
+Restaurant.destroy_all
+
+puts "Starting seed..."
+  10.times do
+    r = Restaurant.create!(
+      name: Faker::Restaurant.name,
+      address: Faker::Address.street_name,
+      category: %w(chinese italian japanese french belgian).sample,
+      )
+    puts "Restaurant id #{r.id} created."
+  end
+
+  puts "Seeding done."
